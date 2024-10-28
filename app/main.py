6e1092b93af12b45a9e1ff1694acec4002a1738b
Victorxs05 <160060912+Victorxs05@users.noreply.github.com>
2024-10-28 12:30:17 +0000

    Update BANCO DE DADOS

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from services.usuario_service import UsuarioService
+from repositories.usuario_repository import UsuarioRepository
+from config.database import Session
+
+def main(): 
+    session = Session()
+    repository = UsuarioRepository(session)
+    service = UsuarioService(repository)
+
+    # Solicitando dados para o usuário.
+    print("\nListando usuários.")
+    nome = input("Digite seu nome: ")
+    email = input("Digite seu e-mail: ")
+    senha = input("Digite sua senha: ")
+
+    service.criar_usuario(nome=nome, email=email, senha=senha)
+
+    #Listar todos os usuarios cadastrados.
+    print("\nListando usuários cadastrado.")
+    lista_usuarios = service.listar_todos_usuarios()
+    for usuario in lista_usuarios:
+        print(f"Nome:{usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
+
+if __name__ == "__main__":
+    main()
